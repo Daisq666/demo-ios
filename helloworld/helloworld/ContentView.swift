559de68c19c509c0c5f74de2fa600b222dac7bff
Daisq666 <1786013726@qq.com>
2020-05-11 16:53:54 +0800

    去掉不需要版本控制的文件

---
+++ helloworld/helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by crland_b on 2020/5/11.
+//  Copyright © 2020 crland_b. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
